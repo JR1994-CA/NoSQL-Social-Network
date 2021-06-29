4562853097ab5f15eb137caac357c862d5794e0b
Duality <jramirezarroyo21@gmail.com>
2021-06-29 04:49:24 -0700

    Schemas and Subdocuments

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,72 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+
+const Reaction = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+          getters: true
+        }
+    }
+);
+
+
 const Thought = new Schema (
     {
         thoughtText: {
             type: String,
             required: true,
-            
-        }
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false  
     }
 
-)
+);
+
+
+Thought.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', Thought);
+
+
+
+module.exports = Thought;
